d403127c392ca2d0a54402815ee9210c01f2446c
Jake Lekas <jlekas33@gmail.com>
2019-09-08 22:00:29 -0400

    Added pills that highlight up when clicked on for the navbar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,4 +4,14 @@ $(document).ready(function() {
         interval: false
     });
     */
+   $('.nav-pills').on('click', 'li', function() {
+       $(this).addClass('active').siblings().removeClass('active')
+   })
+
+   $('.navbar-brand').on('click', function() {
+       console.log('hi');
+       //console.log($('nav-item', '#home'))
+       $('.nav li').removeClass('active');
+       $('#nav-home').addClass('active');
+   })
 })
